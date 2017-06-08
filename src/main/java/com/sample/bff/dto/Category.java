5c6af4826ae025ffe76f90dfb9fa30da0f4cafca
yeyuexia <yyxworld@gmail.com>
2017-06-08 14:17:29 +0800

    add setter and getter

--- src/main/java/com/sample/bff/dto/Category.java
+++ src/main/java/com/sample/bff/dto/Category.java
@@ -5,6 +5,14 @@ public class Category {
     private String name;
     private String description;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
