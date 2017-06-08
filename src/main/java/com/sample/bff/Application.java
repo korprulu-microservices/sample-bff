9ae13bd6168ddb9ef83a0e79658f463440c4509c
yeyuexia <yyxworld@gmail.com>
2017-06-08 14:16:52 +0800

    add eureka

--- src/main/java/com/sample/bff/Application.java
+++ src/main/java/com/sample/bff/Application.java
@@ -3,8 +3,10 @@ package com.sample.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
