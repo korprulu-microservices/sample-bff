953bd7f1bccf695cfd3db2ecdea05b02810cce15
yeyuexia <yyxworld@gmail.com>
2017-06-08 15:30:18 +0800

    add hystrix

--- src/main/java/com/sample/bff/Application.java
+++ src/main/java/com/sample/bff/Application.java
@@ -3,10 +3,12 @@ package com.sample.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/sample/bff/service/CategoryService.java
+++ src/main/java/com/sample/bff/service/CategoryService.java
@@ -1,5 +1,6 @@
 package com.sample.bff.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sample.bff.RibbonConfiguration;
 import com.sample.bff.dto.Category;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -17,7 +18,14 @@ public class CategoryService {
         return new RestTemplate();
     }
 
+    @HystrixCommand(fallbackMethod = "reliable")
     public Category getCategory(String id) {
         return restTemplate().getForObject("http://sample-product/categories/" + id, Category.class);
+    }
+
+    public Category reliable() {
+        final Category category = new Category();
+        category.setName("fallback category");
+        return category;
     }
 }
